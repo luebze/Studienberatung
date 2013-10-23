fc657ef6221ddde545b6cdc0e10b4daa16bc269b
Luebze <luebze@gmail.com>
2013-10-23 18:37:57 +0200

    java-Datei mit Scanner

---
+++ Studienberatung.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Studienberatung {
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Haben sie Abitur?");
+		String input = scan.nextLine();
+		System.out.println(input);
+		
+	}
+}
